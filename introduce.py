3988791107453be59843589992f00ad5989f63fd
py2sellchangfen <357304019@qq.com>
2020-12-03 19:26:50 +0800

    Initial commit

--- introduce.py
+++ introduce.py
@@ -5,9 +5,9 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-img = cv2.imread('pic/destop.jpg')
+# img = cv2.imread('pic/destop.jpg')
 while True:
     success, img = cap.read()
-    cv2.imshow("Capture", img)
+    cv2.imshow("Live", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
