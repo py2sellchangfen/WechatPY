ad5e6a8225ed9847199dad41d641887d2bbbce02
py2sellchangfen <357304019@qq.com>
2020-12-18 22:16:25 +0800

    Initial commit

--- xmain.py
+++ xmain.py
@@ -14,17 +14,24 @@ propath = '/home/h/usr/project/WechatPY'
 if __name__ == '__main__':
     res = requests.get(url, headers)
     res.encoding = 'utf-8'
-    page_text = res.text.replace('<span>','').replace('</span>','')
+    page_text = res.text.replace('<span>','').replace('</span>','')  # 将所有span关键字去掉，可以通过搜索text来确定链接
     soup = BeautifulSoup(page_text, 'lxml')
     chapter = soup.find(attrs={"property": "og:novel:latest_chapter_name"})['content']
-    chapter = int(chapter.replace('第','').replace('章',''))
+    chapter = int(chapter[1:5])
     with open(propath+"/log/xiaoshuo.txt", "r+") as fp:
         prechapter = int(fp.read())
         if chapter > prechapter:
-            url = soup.find_all('a',text='第{}章'.format(prechapter+1))[0]['href']
-            lasturl = 'http://m.78zw.com{}'.format(url)
             fp.seek(0)
             fp.write(str(chapter))
-            sendmessage(user99, "小说已经更新，正在发送最新章节链接\n"+lasturl)
+            try:
+                url = soup.find_all('a',text='第{}章'.format(prechapter+1))[0]['href']
+                showchapter = prechapter + 1
+            except:
+                # url = soup.find_all('a', text='第{}章'.format(chapter))[0]['href']
+                # showchapter = chapter
+                url = soup.find_all('a', text='第{}章 ：'.format(prechapter + 1))[0]['href']
+                showchapter = prechapter + 1
+            lasturl = 'http://m.78zw.com{}'.format(url)
+            sendmessage(user99, "小说已经更新，之前浏览到第{}章，正在发送第{}章链接:\n".format(prechapter, showchapter)+lasturl)
         else:
             lasturl = None
